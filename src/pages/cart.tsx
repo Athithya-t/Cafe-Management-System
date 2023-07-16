5042dcb815c4e5deeca69c2f4083a8eb147ee634
Athithya T <athithya223@gmail.com>
2023-07-16 13:52:11 +0530

    Improves UI,Fixes bugs

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { ItemsType, addButtonClick, addItemsObj, addNoofItems, addTotal, opencartInaction, opencartaction } from 'OrderSlice';
+import { ItemsType, addButtonClick, addItemsObj, addNoofItems, addPrices, addTotal, movedtfromcart, opencartInaction, opencartaction } from 'OrderSlice';
 import React, { useEffect, useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { useSelector,useDispatch } from 'react-redux';
@@ -7,102 +7,85 @@ import { RootState } from 'Store';
 
 const Cart:React.FC = () => {
     const dispatch = useDispatch();
+    const [cartempty,setcartempty] = useState(true);
     const Items = useSelector((state:RootState)=>state.Order);
-    const [cartquantity,setcartquantity] = useState([0]);
     const [carttotal,setcarttotal] = useState(0);
     const [cartui,setcartui] = useState<boolean[]>([]);
-    const [prices,setprices] = useState([0]);
-    const [cartitems,setcartitems] = useState<ItemsType[]>([{Index:0,Name:"",Quantity:0,Price:0}]);
-    let QuantityState = [];
-    let ind:number = 0;
-    let ItemsState:ItemsType[] = [];
+    const [cartitems,setcartitems] = useState<ItemsType[]>([]);
+    let Index:number[] = [];
     let UIState:boolean[] = [];
-    let pricearray:number[] = [];
-    let pricearray1:number[] = [];
-    console.log(prices);
     useEffect(()=>{
-        dispatch(addButtonClick(cartui));
+        dispatch(movedtfromcart(true));
         dispatch(addItemsObj(cartitems));
         dispatch(addTotal(carttotal));
-        dispatch(addNoofItems(cartquantity));
-        if(cartquantity.length!=0)
+        Index = cartitems.filter((i)=> i.Quantity==0).map((i)=>{return i.Index});
+        UIState = [...cartui];
+        Index.forEach((i)=>{UIState[i] = false});
+        dispatch(addButtonClick(UIState));
+        if(carttotal!=0)
         {dispatch(opencartaction(true));
         dispatch(opencartInaction(true));
         }
-        else{dispatch(opencartInaction(false));dispatch(opencartaction(false));}
-    },[cartquantity,cartui])
+        else{dispatch(opencartInaction(false));dispatch(opencartaction(false));setcartempty(true)}
+    },[cartitems]);
     useEffect(()=>{
+        Items.ItemsObj.length==0?null:setcartempty(false);
         setcartui(Items.clickedAdd);
         setcarttotal(Items.Total);
-        pricearray = [...Items.Prices];
-        pricearray1=pricearray.filter((i)=>{return typeof i == 'number'});
-        setprices(pricearray1);
-        setcartquantity(Items.ItemsObj.map(i=>{return i.Quantity}));
         setcartitems(Items.ItemsObj.filter((i)=>{return i.Quantity!=0}));
     },[])
     
     const IncreaseCartQuantity=(index:number)=>{
-        ind = index;
-        QuantityState = [...cartquantity];
-        QuantityState[ind]++;
-        setcartitems(prev=>prev.map((i,index)=>{
+        setcartitems(prev=>prev.map((i,ind)=>{
             if(index===ind){
               return {...i,Quantity:i.Quantity+1};
             }
             return i;
           }))
-        setcartquantity(QuantityState);
-        setcarttotal(prev=>prev+prices[ind]);
+        setcarttotal(prev=>prev+cartitems[index].Price);
     }
     const DecreaseCartQuantity = (index:number)=>{
-        QuantityState = [...cartquantity];
-        QuantityState[index]--;
-        UIState=[...cartui];
-        ItemsState = [...cartitems];
-        if(QuantityState[index]==0){
-            QuantityState.splice(index,1);
-            UIState[ItemsState[index].Index] = false;
-            ItemsState.splice(index,1);
-            setcartui(UIState);
-            setcartquantity(QuantityState);
-            setcartitems(ItemsState);
-            pricearray = [...prices];
-            pricearray.splice(index,1);
-            setprices(pricearray);
-        }
-        setcartitems(prev=>prev.map((i)=>{
-            if(i.Index===index){
-              return {...i,Quantity:i.Quantity-1}
+        setcartitems(prev => prev.map((i, ind) => {
+            if ((ind === index)) {
+              return { ...i, Quantity: i.Quantity - 1 };
             }
             return i;
-          }))
-        setcarttotal(prev=>prev-prices[index]);
-        setcartquantity(QuantityState);
+          }));
+        setcarttotal(prev=>prev-cartitems[index].Price);
     }
     return(
         <>
         <Link href='/home'><Button variant='none' style={{display:"inline"}}>{<img src="./static/arrowback.svg"></img>}</Button></Link>
         <div className='d-flex flex-column align-items-center'>
-            <p style={{display:"inline",fontWeight:'bold',fontSize:'20px'}}>Items in your cart</p>
-            <Card className='shadow-lg animate__animated animate__zoomIn rounded-lg' style={{width:'90%',paddingBottom:'15px'}}>
-            {cartitems.map((item,i)=>{return<> 
-                <div style={{padding:"10px",display:"inline"}}>
-                    <div className='fs-5 fw-bold d-inline'>{item.Name}</div>
-                    <div>Rs.{item.Price}</div>
-                </div>
-                <div className='d-flex flex-row rounded-3 justify-content-center gap-2 shadow-lg d-inline' style={{width:'35%',backgroundColor:'#EFCDF4',marginLeft:'60%',display:'inline',fontWeight:"bold"}}>
-                <button className='rounded ms-2' style={{width:'20px',fontSize:'15px',height:'20px',display:'contents'}} onClick={()=>{IncreaseCartQuantity(i)}}>+</button>
-                <div style={{display:'inline',fontSize:'20px'}}>{cartquantity[i]}</div>
-                <button className='rounded-circle' style={{width:'20px',fontSize:'15px',height:'20px',display:'contents'}} onClick={()=>{DecreaseCartQuantity(i)}}>-</button></div>
-                </>
-            })}
+          <h1 style={{display:"inline",fontWeight:'bold',fontSize:'30px'}}>Cart</h1>
+          {cartempty?<>
+            <h1 style={{marginTop:'50%',color:'purple'}} className='animate__animated animate__zoomIn'>Seems to be empty</h1>
+            <p style={{color:'violet'}} className='animate__animated animate__zoomIn'>Order your favorite food now!</p>
+          </>:
+          <><Card className='shadow-lg animate__animated animate__zoomIn rounded-lg' style={{width:'90%',paddingBottom:'15px'}}>
+          {cartitems.map((item,i)=>{
+            if(item.Quantity!=0){
+              return<> 
+              <div style={{padding:"10px",display:"inline"}}>
+                  <div className='fs-5 fw-bold d-inline'>{item.Name}</div>
+                  <div>Rs.{item.Price}</div>
+              </div>
+              <div className='d-flex flex-row rounded-3 justify-content-center gap-2 shadow-lg d-inline' style={{width:'35%',backgroundColor:'#EFCDF4',marginLeft:'60%',display:'inline',fontWeight:"bold"}}>
+              <button className='rounded-circle' style={{width:'20px',fontSize:'20px',height:'20px',display:'contents'}} onClick={()=>{DecreaseCartQuantity(i)}}>-</button>
+              <div style={{display:'inline',fontSize:'20px'}}>{item.Quantity}</div>
+              <button className='rounded ms-2' style={{width:'20px',fontSize:'20px',height:'20px',display:'contents'}} onClick={()=>{IncreaseCartQuantity(i)}}>+</button>
+              </div>
+              </>
+            }})}
             </Card>
-        </div>
-        <div className='d-flex flex-column align-items-center shadow-lg' style={{position:'fixed',width:'100%',bottom:6}}>
+          <div className='d-flex flex-column align-items-center shadow-lg' style={{position:'fixed',width:'100%',bottom:6}}>
             <Card style={{width:'90%',padding:'2%'}}>
-                <p style={{display:'inline',marginLeft:'5%',fontSize:'20px'}}>Total: <span className='' style={{marginLeft:'50%',fontWeight:'bolder'}}>Rs {carttotal}</span></p>
+                <p style={{display:'inline',marginLeft:'5%',fontSize:'20px',fontWeight:'bold'}}>Total: <span className='' style={{marginLeft:'50%',fontWeight:'bolder'}}>Rs {carttotal}</span></p>
                 <Button variant='none' size='sm' className='rounded-lg' style={{backgroundColor:'#EFCDF4',width:'100%',height:'50px',fontWeight:'bolder',marginLeft:'0%'}}>Proceed to Pay</Button>
             </Card>
+          </div>
+          </>
+          }
         </div>
         </>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,23 @@ import { Alert, ProgressBar } from "react-bootstrap";
 import Cafe from 'static/Cafe.jpg';
 import Image from "next/image";
 import 'animate.css';
+import { TailSpin } from 'react-loader-spinner';
+
+export const Loader:React.FC = ()=>{
+    return(
+        <div className='d-flex flex-column align-items-center'>
+            <h1 style={{marginTop:'40%'}}>Loading</h1>
+            <TailSpin height="30%" width="30%" color="purple" radius="1" visible={true} wrapperStyle={{marginLeft:'40%'}}/>  
+        </div>
+    )
+}
 
 const Login:React.FC=()=>{
     const [Name,setName] = useState("");
     const [MobileNo,setMobileNo] = useState("");
     const [ComponentRendered,setComponentRendered] = useState(false);
     const [sendError,setsendError] = useState(false);
+    const Formlabel = {fontWeight:'bold',fontSize:'20px',marginTop:'3%'}
     const submitHandler =()=>{
         if((MobileNo!="")&&(Name!="")){
         localStorage.setItem('user',JSON.stringify({Name,MobileNo}));
@@ -20,11 +31,6 @@ const Login:React.FC=()=>{
         else{
             setsendError(prev=>!prev);
         }
-    }
-    function Loader(){
-        const [time,setTime] = useState(90);
-        setTimeout(()=>{setTime(time+100)},5)
-        return(<ProgressBar now={time}/>)
     }
     useEffect(()=>{
         setTimeout(()=>{setComponentRendered(true)},600);
@@ -36,23 +42,20 @@ const Login:React.FC=()=>{
         <>
             {ComponentRendered ?
             <> 
-            <Form>
-                <Image src={Cafe} alt="Cafe Image" style={{width:'95%',height:'50%'}}/>
-                {sendError&&<div className="animate__animated animate__flash"><Alert variant="danger" style={{width:'100%'}} className="animate__animated animate__headShake">Please enter your Name and Mobile No to proceed</Alert></div>}
+            <Form className='d-flex flex-column align-items-center'>
+                <Image src={Cafe} alt="Cafe Image" style={{width:'75%',height:'30%'}}/>
+                {sendError&&<div className="animate__animated animate__flash"><Alert variant="danger" style={{width:'100%'}} className="animate__animated animate__headShake">Please enter your Name and Mobile Number to proceed</Alert></div>}
                 <InputGroup className="d-flex flex-column align-items-center gap-2" hasValidation>
                     <h1>Scan! Order! Chill!</h1>
-                    <Form.Label>Enter Name</Form.Label>
-                    <Form.Control placeholder="Name" className="w-75 rounded-pill p-3" required onChange={(e)=>{setName(e.target.value)}}></Form.Control>
-                    <Form.Label>Enter Mobile No</Form.Label>
-                    <Form.Control placeholder="Mobile No" className="w-75 rounded-pill p-3" required onChange={(e)=>{setMobileNo(e.target.value)}}></Form.Control>
+                    <Form.Label style={Formlabel}>Enter Name</Form.Label>
+                    <Form.Control placeholder="Name" className="w-75 rounded-4 p-3" required onChange={(e)=>{setName(e.target.value)}}></Form.Control>
+                    <Form.Label style={Formlabel}>Enter Mobile No</Form.Label>
+                    <Form.Control placeholder="Mobile No" className="w-75 rounded-4 p-3" required onChange={(e)=>{setMobileNo(e.target.value)}}></Form.Control>
                     {MobileNo.match(/[a-z]/)&&<Form.Text style={{color:"red"}}>Mobile Number must contain numbers from 0-9</Form.Text>}
-                    <Button className="rounded-pill p-2" variant="success" onClick={submitHandler}>Submit</Button>
+                    <Button className="rounded-4" variant='none' style={{backgroundColor:'purple',color:'white',marginTop:'3%',padding:'10px'}} onClick={submitHandler}>Submit</Button>
                 </InputGroup>
             </Form>
-            </>:<div>
-                <h1>Loading</h1>
-                <Loader/>
-                </div>}
+            </>:<Loader/>}
         </>
     )
 }
