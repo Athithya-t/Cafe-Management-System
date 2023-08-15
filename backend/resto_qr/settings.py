6ef409e14a3f7429f317cb2e52fe625e45e5a36b
Athithya T <athithya223@gmail.com>
2023-08-15 13:07:34 +0530

    Modifies settings.py

--- backend/resto_qr/settings.py
+++ backend/resto_qr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l=127_#1k8&s!t&c#b%!giwom2q2*$mic!7)q5sc4ymxyy8p6c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://localhost:3000','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['http://localhost:3000','127.0.0.1','localhost','vercel.app']
 
 
 # Application definition
@@ -140,7 +140,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
  
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:3000",'https://cafe-management-system.vercel.app'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
